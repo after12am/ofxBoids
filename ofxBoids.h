b5388546553713ee4c6f748809028c17b0051f4e
satoshi okami <me.after.12am@gmail.com>
2012-02-29 16:54:33 +0900

    ヘッダーファイルをまとめる

---
+++ ofxBoids.h
@@ -0,0 +1,15 @@
+//
+//  ofxBoids.h
+//  emptyExample
+//
+//  Created by Okami Satoshi on 12/02/29.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef _ofxBoids_h
+#define _ofxBoids_h
+
+#include "Vehicle.h"
+#include "SteeredVehicle.h"
+
+#endif
